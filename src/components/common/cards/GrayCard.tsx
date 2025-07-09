93362661bb8532a36703f41be2a1449d4cc8e65f
magedElshafey <magedelshafey98@gmail.com>
2025-07-09 11:39:57 +0300

    up

--- src/components/common/cards/GrayCard.tsx
+++ src/components/common/cards/GrayCard.tsx
@@ -5,7 +5,7 @@ interface GrayCardProps {
 }
 const GrayCard: React.FC<GrayCardProps> = ({ data }) => {
   return (
-    <div className="bg-[#e5e6eb] min-h-[270px] w-full p-7 flex flex-col items-center justify-center rounded-lg relative">
+    <div className="bg-[#e5e6eb] min-h-[180px] lg:min-h-[270px] w-full p-7 flex flex-col items-center justify-center rounded-lg relative">
       {/* <p className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-7xl text-white font-black z-10 bg-opacity-25 opacity-65">
         {++index}
       </p> */}
